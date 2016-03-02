ab89448947e9fe971369b1068e2732166c580af5
Sinatra <root@stackin.money>
2016-03-01 21:44:18 -0500

    Update receiver.ino

--- wireless/receiver.ino
+++ wireless/receiver.ino
@@ -7,15 +7,16 @@ void setup() {
   Serial.begin(9600);
   focus.attach(7);
   vw_set_rx_pin(6); 
-  vw_setup(2000);
-  vw_rx_start();  
+  vw_setup(4000);
+  vw_rx_start();
 }
 void loop() {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
   if (vw_get_message(buf, &buflen)) {
-    Serial.println(buf[0]);
-    focus.write(buf[0]);
+    int value = map(buf[0], 0, 180, 0, 2000);
+    Serial.println(value);
+    //focus.write(value);
   }
 }
