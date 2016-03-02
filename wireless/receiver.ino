2d8c2718b6bed2050b02115c00cf29dc2b18d6b4
Sinatra <root@stackin.money>
2016-03-01 20:42:17 -0500

    Update receiver.ino

--- wireless/receiver.ino
+++ wireless/receiver.ino
@@ -2,23 +2,20 @@
 #include <ServoTimer2.h>
 
 ServoTimer2 focus;
-void setup()
-{
+
+void setup() {
   Serial.begin(9600);
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_set_rx_pin(6);
-    vw_setup(2000);  // Bits per sec
-    focus.attach(7);
-    vw_rx_start();       // Start the receiver PLL running
+  focus.attach(7);
+  vw_set_rx_pin(6); 
+  vw_setup(2000);
+  vw_rx_start();  
 }
-    void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+void loop() {
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-    if (vw_get_message(buf, &buflen)) {
-      Serial.println(buf[0]);
-      focus.write(buf[0] - '0');
-    }
-
+  if (vw_get_message(buf, &buflen)) {
+    Serial.println(buf[0]);
+    focus.write(buf[0]);
+  }
 }
