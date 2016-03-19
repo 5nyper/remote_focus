e7109501582a6468909d78e8be91d84be9aef7a2
Sinatra <root@stackin.money>
2016-03-19 12:22:26 -0400

    add speed control

--- src/receiver.ino
+++ src/receiver.ino
@@ -2,6 +2,8 @@
 #include <ServoTimer2.h>
 
 ServoTimer2 focus;
+bool toggle = false;
+int spe = 10;
 
 void setup() {
   Serial.begin(9600);
@@ -10,13 +12,76 @@ void setup() {
   vw_setup(4000);
   vw_rx_start();
 }
+
 void loop() {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
   if (vw_get_message(buf, &buflen)) {
-    int value = map(buf[0], 0, 180, 500, 2500);
-    Serial.println(value);
-    focus.write(value);
+    if (buf[0] == 200) {
+      delay(1000);
+      toggle = true;
+      presetMode();
+    }
+    else {
+      int value = map(buf[0], 10, 190, 500, 2500);
+      Serial.print("value: ");
+      Serial.println(value);
+      focus.write(value);
+    }
+  }
+}
+
+void presetMode() {
+  while(toggle == true) {
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  
+    if (vw_get_message(buf, &buflen)) {
+      if (buf[0] <= 10) {
+        spe = buf[0];
+        Serial.print("spe: ");
+        Serial.println(spe);
+      }
+      else {
+        int value = map(buf[0], 10, 190, 500, 2500);
+          Serial.println(value);
+          speed(value, spe);
+      }
+      if (buf[0] == 201) {
+        toggle = false;
+        delay(1000);
+        break;
+      }
+    }
+  }
+}
+
+void speed(int value, int inc) {
+  if (value > focus.read()) {
+    while (true) {
+      if (focus.read() == 2500 || focus.read() + inc >= value)
+        break;
+      else {
+        Serial.print("read: ");
+        Serial.print(focus.read());
+        Serial.print(" value: ");
+        Serial.println(focus.read() + inc);
+        focus.write(focus.read() + inc);
+      }
+    }
+  }
+  else {
+    while (true) {
+      if (focus.read() == 500 || focus.read() - inc <= value)
+        break;
+      else {
+        Serial.print("read: ");
+        Serial.print(focus.read());
+        Serial.print(" value: ");
+        Serial.println(focus.read() - inc);
+        focus.write(focus.read() - inc);
+      }
+    }
   }
 }
